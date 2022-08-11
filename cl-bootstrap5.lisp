00a05f2bf147ceb770e1308983537cda37e94427
FluxGenerator <loki_was_here@hotmail.com>
2022-08-11 15:30:01 +0530

    Added Bootstrap column and row.

--- cl-bootstrap5.lisp
+++ cl-bootstrap5.lisp
@@ -4,5 +4,24 @@
     "Bootstrap container."
     `(with-html-output (*standard-output*)
 	 (:div :class (concatenate 'string "container"
-				   (if ,type (format nil "-~a" ,type))) ,@body))))
+				   (if ,type (format nil "-~a" ,type)))
+	       ,@body)))
 
+(defmacro bs-col ((&key (width nil) (breakpoint nil) (auto nil)) &body body)
+    "Bootstrap column."
+    `(with-html-output(*standard-output*)
+	 (:div :class (concatenate 'string "col"
+				   (if ,width (format nil "-~a" ,size))
+				   (if ,breakpoint
+				       (if ,auto
+					   (format nil " col-~a-auto" ,breakpoint)
+					   (format nil " col-~a" ,breakpoint))))
+	       ,@body)))
+
+(defmacro bs-row ((&key (row-column nil)) &body body)
+    "Bootstrap row."
+    `(with-html-output (*standard-output*)
+	 (:div :class (concatenate 'string "row"
+				   (if ,row-column
+				       (format nil " row-cols-~a" ,row-column)))
+	       ,@body)))
--- package.lisp
+++ package.lisp
@@ -1,4 +1,7 @@
 (in-package :cl-user)
 (defpackage #:cl-bootstrap5
   (:use :cl :cl-user :cl-who)
-  (:export #:bs-container))
+  (:export #:bs-container)
+  (:export #:bs-container
+	   #:bs-col
+	   #:bs-row))
