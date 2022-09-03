5034cebcaf3ab213443d9b18ab67a79ced225509
FluxGenerator <loki_was_here@hotmail.com>
2022-09-03 11:33:38 +0530

    Added accordian header.

--- src/accordion.lisp
+++ src/accordion.lisp
@@ -11,3 +11,19 @@
   `(with-html-output (*standard-output*)
      (:div :class "accordion-item" :id ,id
 	   ,@body)))
+
+(defmacro bs-accordion-header ((&key (h 2) (id "")) &body body)
+  "Bootstrap accordion header."
+  (case h
+    (1 `(with-html-output (*standard-output*)
+	 (:h1 :class "accordion-header" :id ,id ,@body)))
+    (2 `(with-html-output (*standard-output*)
+	 (:h2 :class "accordion-header" :id ,id ,@body)))
+    (3 `(with-html-output (*standard-output*)
+	 (:h3 :class "accordion-header" :id ,id ,@body)))
+    (4 `(with-html-output (*standard-output*)
+	 (:h4 :class "accordion-header" :id ,id ,@body)))
+    (5 `(with-html-output (*standard-output*)
+	  (:h5 :class "accordion-header" :id ,id ,@body)))
+    (6 `(with-html-output (*standard-output*)
+	  (:h6 :class "accordion-header" :id ,id ,@body)))))
