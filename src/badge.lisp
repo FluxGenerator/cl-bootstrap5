0fd83b66c2390f3eb38d420b7225e544630aa308
FluxGenerator <loki_was_here@hotmail.com>
2022-09-11 12:25:46 +0530

    Fixed badge not showing up.

--- src/badge.lisp
+++ src/badge.lisp
@@ -1,11 +1,11 @@
 (in-package :cl-bootstrap5)
 
-(defmacro bs-badge ((&key (id "") (type nil)
+(defmacro bs-badge ((&key (id "") (type "secondary")
 			  (rounded-pill nil)) &body body)
   "Bootstrap badge."
   `(bs
      (:span :id ,id :class (concatenate 'string
 					"badge"
-					(if ,type (format nil " badge-~a" ,type))
+					(format nil " bg-~a" ,type)
 					(if ,rounded-pill (format nil " rounded-pill")))
 	    ,@body)))
